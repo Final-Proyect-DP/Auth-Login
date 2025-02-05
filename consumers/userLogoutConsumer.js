aef49735ffe9c58079ca413886af60af21b1939b
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-02-05 14:30:02 -0500

    updating yml and dockerfile

--- consumers/userLogoutConsumer.js
+++ consumers/userLogoutConsumer.js
@@ -17,25 +17,25 @@ const run = async () => {
       eachMessage: async ({ topic, partition, message }) => {
         try {
           const encryptedMessage = JSON.parse(message.value.toString());
-          logger.info('Mensaje cifrado recibido:', encryptedMessage);
+          logger.info('Crypted message:', encryptedMessage);
           
           const decryptedMessage = userService.decryptMessage(encryptedMessage);
           
           if (!decryptedMessage || !decryptedMessage.userId) {
-            throw new Error('Mensaje descifrado inválido o userId no encontrado');
+            throw new Error('invalid uncrypted message or userId not found');
           }
           
           logger.info(`Procesando cierre de sesión para usuario: ${decryptedMessage.userId}`);
           await redisUtils.deleteToken(decryptedMessage.userId);
-          logger.info(`Token eliminado exitosamente para usuario: ${decryptedMessage.userId}`);
+          logger.info(`Token delete: ${decryptedMessage.userId}`);
 
         } catch (error) {
-          logger.error('Error procesando mensaje de cierre de sesión:', error.message);
+          logger.error('Error prosecing message:', error.message);
         }
       },
     });
   } catch (error) {
-    logger.error('login Consumer: Error iniciando el consumidor:', error);
+    logger.error('login Consumer: Error starting consumer:', error);
     throw error;
   }
 };
